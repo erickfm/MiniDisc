59843afd4b3a02e95c58f9fb4e62eb4d14e3fa10
Erick Martinez <erick@Ericks-MBP.lan>
2022-09-17 15:37:52 -0700

    testing oauth again

--- app.py
+++ app.py
@@ -7,34 +7,19 @@ st.set_page_config(
     page_icon=":minidisc:"
 )
 
-# auth_manager = SpotifyOAuth(
-#         client_id=client_id,
-#         client_secret=client_secret,
-#         scope=scope,
-#         redirect_uri=redirect_uri,
-#         # open_browser=False
-#     )
-# r = requests.get(auth_manager.get_authorize_url())
-# auth = auth_manager.get_access_token(r.url, as_dict=False)
-# spotipy.Spotify(auth = auth)
-# # auth_manager.get_auth_response()
-#
-#
-# run sidebar based control function
-sidebar()
-# code = st.experimental_get_query_params()['code']
-# st.write(code)
+# # run sidebar based control function
+# sidebar()
 
-# scope = "user-read-playback-state user-read-currently-playing user-library-read user-top-read user-read-recently-played playlist-read-private playlist-read-collaborative"
-# redirect_uri= 'https://example.com/callback/'
-# client_id = 'd3c4d26f750f45daa2434a1585745291'
-# client_secret = '8912e43e7ebe4b1a84097e3997eb50ec'
+scope = "user-read-playback-state user-read-currently-playing user-library-read user-top-read user-read-recently-played playlist-read-private playlist-read-collaborative"
+redirect_uri= 'https://example.com/callback/'
+client_id = 'd3c4d26f750f45daa2434a1585745291'
+client_secret = '8912e43e7ebe4b1a84097e3997eb50ec'
 
-# auth_manager=SpotifyOAuth(client_id=client_id,
-#                           client_secret=client_secret,
-#                           scope=scope,
-#                           redirect_uri=redirect_uri,
-#                          )
-# sp = spotipy.Spotify(auth_manager=auth_manager)
-# for i in sp.current_user_top_tracks(10)['items']:
-#     st.write(i['name'])
+auth_manager=SpotifyOAuth(client_id=client_id,
+                          client_secret=client_secret,
+                          scope=scope,
+                          redirect_uri=redirect_uri,
+                         )
+sp = spotipy.Spotify(auth_manager=auth_manager)
+for i in sp.current_user_top_tracks(10)['items']:
+    st.write(i['name'])
