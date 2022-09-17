f416e7f1fdb426b5db8388e9ff112971bc6ff103
Erick Martinez <erick@Ericks-MBP.lan>
2022-09-17 15:41:05 -0700

    test oauth again

--- app.py
+++ app.py
@@ -10,10 +10,10 @@ st.set_page_config(
 # # run sidebar based control function
 # sidebar()
 
-scope = "user-read-playback-state user-read-currently-playing user-library-read user-top-read user-read-recently-played playlist-read-private playlist-read-collaborative"
-redirect_uri= 'https://example.com/callback/'
-client_id = 'd3c4d26f750f45daa2434a1585745291'
-client_secret = '8912e43e7ebe4b1a84097e3997eb50ec'
+# scope = "user-read-playback-state user-read-currently-playing user-library-read user-top-read user-read-recently-played playlist-read-private playlist-read-collaborative"
+# redirect_uri= 'https://example.com/callback/'
+# client_id = 'd3c4d26f750f45daa2434a1585745291'
+# client_secret = '8912e43e7ebe4b1a84097e3997eb50ec'
 
 auth_manager=SpotifyOAuth(client_id=client_id,
                           client_secret=client_secret,
