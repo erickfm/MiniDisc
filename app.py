81d7407a7d34859bc4210fd5017d302631027791
Erick Martinez <erick@Ericks-MBP.lan>
2022-09-18 15:45:51 -0700

    hopefully oauth works now

--- app.py
+++ app.py
@@ -1,25 +1,37 @@
 from playlist_tool.components import *
 
+auth_manager = SpotifyOAuth(client_id=client_id,
+                                client_secret=client_secret,
+                                scope=scope,
+                                redirect_uri=redirect_uri,
+                                )
+try:
+    code = st.experimental_get_query_params()['code']
+    sp = spotipy.Spotify(auth=auth_manager.get_access_token(code, as_dict=False))
+    tracks = sp.current_user_top_tracks(10)['items']
+    st.write(f'### {tracks}')
+
+except Exception as e:
+    st.write(e)
+    authorization_url = auth_manager.get_authorize_url()
+    st.write(f'''<h1>
+        Please login using this <a target="_self"
+        href="{authorization_url}">url</a></h1>''',
+             unsafe_allow_html=True)
+
+
 # set page configuration
-st.set_page_config(
-    layout="wide",
-    page_title=title,
-    page_icon=":minidisc:"
-)
+# st.set_page_config(
+#     layout="wide",
+#     page_title=title,
+#     page_icon=":minidisc:"
+# )
 
 # # run sidebar based control function
 # sidebar()
 
-# scope = "user-read-playback-state user-read-currently-playing user-library-read user-top-read user-read-recently-played playlist-read-private playlist-read-collaborative"
-# redirect_uri= 'https://example.com/callback/'
-# client_id = 'd3c4d26f750f45daa2434a1585745291'
-# client_secret = '8912e43e7ebe4b1a84097e3997eb50ec'
 
-auth_manager=SpotifyOAuth(client_id=client_id,
-                          client_secret=client_secret,
-                          scope=scope,
-                          redirect_uri=redirect_uri,
-                         )
-sp = spotipy.Spotify(auth_manager=auth_manager)
-for i in sp.current_user_top_tracks(10)['items']:
-    st.write(i['name'])
+
+
+
+# ?code=AQAjdrurVFXyJvhRStRssqYIkR60RIz2NGKjkZvXAI6pgYUsifbhip_b2nsAjf94xxe-GMCopE8Fq9zvvCkDss9UudlcPI_QBmO8pdnlmEzrQtIsxIkrtQj9FMhZVVRws0kwz4VgQEjhcuJWFgHZ3hO93OAAJIwouybIMCE6_Q7n3R80mFVyqTjThJMFIeyxQ33VDjAX2Sbe1o2MKE-1e6ALq7BdHuBI5G9ZViAsu2hHdjRws-dAX9iSF6kAtnkGV8RErBQhN1ibIs2cL1UQ7922DVnjvCc4JrrP2n1i2f-flsqhnAIjOQ4ntpkR3ok_rt7RmLgMaBPJCTBa5xAbHBvg3RsR9U1Ze-wdvZ0gvo_pn_Qs7z4ZAUx19OPeMdDjuivX-gyJZYlA#_=_
